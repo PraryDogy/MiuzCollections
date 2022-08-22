b1894a1e26fa98f3c6fbece656dc9d57bd754ca9
Evgeny Loshkarev <evlosh@gmail.com>
2022-08-22 13:43:53 +0300

    minor changes

--- testfile.py
+++ testfile.py
@@ -1,25 +1,13 @@
 import tkinter
 import cfg
 
-class Lbutton(tkinter.Label):
-    def __init__(self, master, cmd, txt):
-        tkinter.Label.__init__(self, master)
-
-        self.config(bg=cfg.BGBUTTON, fg=cfg.FONTCOLOR, 
-            width=17, height=2, text=txt)
-        self.bind('<Button-1>', cmd)
+class TkObjects:
+    genFrame = 'tkinter frame with general settings'
+    expFram = 'tkinter frame with expert settings'
+    genBtn = 'tkinter button'
+    expBtn = 'tkinter button'
 
 
-root = tkinter.Tk()
+TkObjects.genBtn = 'hi'
 
-frame = tkinter.Frame(root, bg='red', width=50, height=50)
-frame.pack(side='left')
-
-frame2 = tkinter.Frame(root, bg='blue', width=150, height=150)
-frame2.pack(side='right')
-
-
-b = Lbutton(frame2, lambda event: root.destroy(), 'hello')
-b.pack()
-
-root.mainloop()
+print(TkObjects.genBtn)
