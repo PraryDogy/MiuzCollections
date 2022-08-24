1abf64a703c9c479615f423e9bf62a7a1198c0f5
evlosh <evlosh@gmail.com>
2022-08-24 22:25:30 +0300

    kwargs for styled tkinters

--- Utils/Styled.py
+++ Utils/Styled.py
@@ -3,28 +3,29 @@ import cfg
 
 class MyButton(tkinter.Label):
     """Tkinter Label with binded function to 
-    mouse left click and cfg styles."""
+    mouse left click and cfg styles.
+    Methods: Cmd"""
 
-    def __init__(self, master, cmd, txt):
+    def __init__(self, master, **kwargs):
+        tkinter.Label.__init__(self, master, **kwargs)
+        self.configure(
+            bg=cfg.BGBUTTON, fg=cfg.FONTCOLOR, 
+            width=17, height=2)
 
-        tkinter.Label.__init__(self, master)
-
-        self.configure(bg=cfg.BGBUTTON, fg=cfg.FONTCOLOR, 
-            width=17, height=2, text=txt)
+    def Cmd(self, cmd):
+        """Bind tkinter label to left click"""
         self.bind('<Button-1>', cmd)
 
 
 class MyFrame(tkinter.Frame):
     """Tkinter Frame with cfg styles"""
-    def __init__(self, master):
-        tkinter.Frame.__init__(self, master)
-
+    def __init__(self, master, **kwargs):
+        tkinter.Frame.__init__(self, master, **kwargs)
         self.configure(bg=cfg.BGCOLOR)
 
 
 class MyLabel(tkinter.Label):
     """Tkinter Label with cfg styles"""
-    def __init__(self, master):
-        tkinter.Label.__init__(self, master)
-
+    def __init__(self, master, **kwargs):
+        tkinter.Label.__init__(self, master, **kwargs)
         self.configure(bg=cfg.BGCOLOR, fg=cfg.FONTCOLOR)
