ed680ac18ec0c7c566e5a8ae92cace67fd31f5c9
evlosh <evlosh@gmail.com>
2022-08-24 22:26:37 +0300

    .

--- testfile.py
+++ testfile.py
@@ -1,8 +1,1 @@
-from smb.SMBConnection import SMBConnection
-import subprocess
-import cfg
-import os
-
-
-parent = os.path.dirname(__file__)
-os.rmdir(os.path.join(parent, '.eggs'))
+import tkinter
