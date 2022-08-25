0c5413854d0f8e5337dfe1ca58700f69c5274dfd
Evgeny Loshkarev <evlosh@gmail.com>
2022-08-25 10:22:25 +0300

    .

--- testfile.py
+++ testfile.py
@@ -1,1 +1,14 @@
+import os
 import tkinter
+import shutil
+import cfg
+
+
+lib = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), 
+    'files', 
+    'lib'
+    )
+
+
+print(os.path.exists(lib))
