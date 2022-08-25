6d6353c7e7c1697dea1d1eea1c0a6f13aa79264f
Evgeny Loshkarev <evlosh@gmail.com>
2022-08-25 10:20:01 +0300

    setup can copy lib folder in .app and rename dist to _Dist

--- setup.py
+++ setup.py
@@ -8,28 +8,30 @@ import os
 import shutil
 
 from setuptools import setup
+import cfg
 
-from cfg import APP_VER
+
 
 APP = ['start.py']
 DATA_FILES = [os.path.join(
     os.path.dirname(__file__), 
     'Gallery/Settings/upd.jpg')
               ]
+
 OPTIONS = {
     'iconfile': 'icon.icns',
     'plist': {
-    'CFBundleName': 'MiuzGallery',
-    'CFBundleShortVersionString':APP_VER, 
-    'CFBundleVersion': APP_VER, 
-    'CFBundleIdentifier':'com.evlosh.MiuzGallery', 
+    'CFBundleName': cfg.APP_NAME,
+    'CFBundleShortVersionString':cfg.APP_VER, 
+    'CFBundleVersion': cfg.APP_VER, 
+    'CFBundleIdentifier':f'com.evlosh.{cfg.APP_NAME}', 
     'NSHumanReadableCopyright': 'Created by Evgeny Loshkarev\nCopyright © 2022 MIUZ Diamonds. All rights reserved.'
     }
     }
 
 setup(
     app=APP,
-    name='MiuzGallery',
+    name=cfg.APP_NAME,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
@@ -45,3 +47,22 @@ setup(
 parent = os.path.dirname(__file__)
 shutil.rmtree(os.path.join(parent, 'build'))
 shutil.rmtree(os.path.join(parent, '.eggs'))
+
+
+lib = '/Users/Loshkarev/Documents/Разное/Projects/files/lib'
+
+pasteHere = os.path.join(
+    os.path.dirname(__file__), 
+    'dist', 
+    f'{cfg.APP_NAME}.app', 
+    'Contents', 
+    'lib'
+    )
+
+shutil.copytree(lib, pasteHere)
+
+inName = os.path.join(os.path.dirname(__file__), 'dist')
+outName = os.path.join(os.path.dirname(__file__), '__Dist')
+os.rename(inName, outName)
+
+
