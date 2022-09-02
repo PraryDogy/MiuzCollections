a6e3a4e1b61c3b32ffc70e8660194c81ee988d75
Evgeny Loshkarev <evlosh@gmail.com>
2022-09-02 17:06:57 +0300

    linting

--- Gallery/image_viewer.py
+++ Gallery/image_viewer.py
@@ -2,7 +2,6 @@
 Gui for view image.
 """
 
-from datetime import datetime
 import os
 import subprocess
 import tkinter
@@ -18,24 +17,28 @@ class Globals:
     Stores variables
     """
     src = str()
-    
-    
+
+
 class ImagePreview(tkinter.Toplevel):
+    """
+    Creates new window (tkinter Top Level) with image & buttons.
+    * param src: source path of image.
+    """
     def __init__(self, src):
         if src is None:
             return
-        
+
         Globals.src = src
-        
+
         tkinter.Toplevel.__init__(self)
         self.withdraw()
-        self.protocol("WM_DELETE_WINDOW", lambda: self.destroy())
+        self.protocol("WM_DELETE_WINDOW", self.destroy)
         self.bind('<Command-w>', lambda e: self.destroy())
         self.configure(bg=cfg.BGCOLOR, padx=15, pady=15)
-        
+
         if os.path.exists(Globals.src):
             cfg.ROOT.update_idletasks()
-            ImageFrame(self)
+            ImageFrame(self).pack(expand=True, fill=tkinter.BOTH)
         else:
             ImageError(self)
 
@@ -48,22 +51,29 @@ class ImagePreview(tkinter.Toplevel):
 
 
 class ImageFrame(MyLabel):
+    """
+    Creates tkinter label with image 0.8 wight, height of screen.
+    * param master: tkinter toplevel.
+    """
     def __init__(self, master):
-        imgSrc = Image.open(Globals.src)
-        imgCopy= imgSrc.copy()
+        img_src = Image.open(Globals.src)
+        img_copy= img_src.copy()
 
-        h = int(cfg.ROOT.winfo_screenheight()*0.8)
-        w = int(cfg.ROOT.winfo_screenwidth()*0.8)
-        imgCopy.thumbnail((w, h))
+        screen_h = int(cfg.ROOT.winfo_screenheight()*0.8)
+        screen_w = int(cfg.ROOT.winfo_screenwidth()*0.8)
+        img_copy.thumbnail((screen_w, screen_h))
 
-        imgTk = ImageTk.PhotoImage(imgCopy)
+        img_tk = ImageTk.PhotoImage(img_copy)
 
-        MyLabel.__init__(self, master, image=imgTk)
-        self.image_names = imgTk
-        self.pack(expand=True, fill=tkinter.BOTH)
+        MyLabel.__init__(self, master, image=img_tk)
+        self.image_names = img_tk
 
 
 class ImageError(MyLabel):
+    """
+    Creates tkinter label with error message.
+    * param master: tkinter toplevel
+    """
     def __init__(self, master):
         txt = (
             'Не могу открыть изображение. Возможные причины:'
@@ -76,35 +86,53 @@ class ImageError(MyLabel):
 
 
 class NamePathFrame(MyFrame):
+    """
+    Creates tkinter frame.
+    * param master: tkinter top level.
+    """
     def __init__(self, master):
         MyFrame.__init__(self, master)
         self.pack(pady=(15, 15))
         NamePathTitle(self)
         NamePathButton(self)
-        
+
 
 class NamePathTitle(MyLabel):
+    """
+    Creates tkinter label with src path to img.
+    * param master: tkinter frame.
+    """
     def __init__(self, master):
         MyLabel.__init__(
-            self, master,
-            text=Globals.src.replace(cfg.PHOTO_DIR, '')
-            )
+            self, master, text=Globals.src.replace(cfg.PHOTO_DIR, ''))
         self.pack(side=tkinter.LEFT)
 
-          
+
 class NamePathButton(MyButton):
+    """
+    Creates tkinter button with copy img path fuction.
+    * param master: tkinter frame.
+    """
     def __init__(self, master):
-        MyButton.__init__(self, master, text='Копировать имя')        
+        MyButton.__init__(self, master, text='Копировать имя')
         self.configure(height=1)
-        self.Cmd(lambda e: self.copyToClipboard())
+        self.Cmd(lambda e: self.copy_name())
         self.pack(side=tkinter.LEFT, padx=(15, 0))
 
-    def copyToClipboard(self):
+    def copy_name(self):
+        """
+        Copies path to folder with image.
+        Simulates button press with color.
+        """
         self.Press()
         my_copy(Globals.src.split('/')[-1].split('.')[0])
 
 
 class OpenCloseFrame(MyFrame):
+    """
+    Creates tkinter frame for open and close buttons.
+    * param master: tkinter frame.
+    """
     def __init__(self, master):
         MyFrame.__init__(self, master)
         self.pack()
@@ -114,21 +142,33 @@ class OpenCloseFrame(MyFrame):
 
 
 class OpenButton(MyButton):
+    """
+    Creates tkinter button with open image folder function.
+    * param master: tkinter frame.
+    """
     def __init__(self, master):
         MyButton.__init__(self, master, text='Открыть папку')
         self.configure(height=2)
-        self.Cmd(lambda e: self.OpenFolder())
+        self.Cmd(lambda e: self.open_folder())
         self.pack(side=tkinter.LEFT, padx=(0, 15))
-        
-    def OpenFolder(self):
+
+    def open_folder(self):
+        """
+        Opens folder with image.
+        Simulates button press with color.
+        """
         self.Press()
         path = '/'.join(Globals.src.split('/')[:-1])
         subprocess.check_output(["/usr/bin/open", path])
-        
+
 
 class CloseButton(MyButton):
+    """
+    Creates tkinter button with close current toplevel function.
+    * param master: tkinter frame.
+    """
     def __init__(self, master):
         MyButton.__init__(self, master, text='Закрыть')
         self.configure(height=2)
-        self.Cmd(lambda e: self.winfo_toplevel().destroy())        
+        self.Cmd(lambda e: self.winfo_toplevel().destroy())
         self.pack(side=tkinter.RIGHT)
--- testfile.py
+++ testfile.py
@@ -1,250 +1,61 @@
-import datetime
-import os
-import sys
-
-import sqlalchemy
+import re
+import tkinter
+import traceback
+from datetime import datetime
+from tkinter.ttk import Separator
+from turtle import width
 
 import cfg
-from admin import print_alive
-from database import Thumbs, Dbase
-from Utils.Utils import create_thumb
+import cv2
+import numpy
+import sqlalchemy
+import tkmacosx
+from database import Config, Dbase, Thumbs
+from PIL import Image, ImageTk
+from Utils.Styled import MyButton, MyFrame, MyLabel
+
+class Globals:
+    """
+    Variables for current module
+    """
+
+    __getCurrColl = sqlalchemy.select(Config.value).where(
+        Config.name=='currColl')
+    currColl = Dbase.conn.execute(__getCurrColl).first()[0]
+
+    # bind reset function for thumbnails frame: Images().Reset()
+    images_reset = object
 
 
-class BaseScan(list):    
-    def __init__(self, aged=True):
-        """List of dirs in folders with 
-        year name (year name is 2018, 2020 etc). 
-        :param bool aged: if true removes dirs with time created
-        earlier than number of days ago (cfg.FILE_AGE)""" 
-    
-        photoDir = os.path.join(os.sep, *cfg.PHOTO_DIR.split('/')) 
-        __baseDirs = list()
+size = Dbase.conn.execute(
+    sqlalchemy.select(Config.value).where(
+        Config.name=='size')).first()[0]
+size = int(size)
+img = Thumbs.__dict__[f'img{size}']
 
-        for r in range(2018, datetime.datetime.now().year + 1):
-            yearDir = os.path.join(photoDir, str(r))
-            __baseDirs.append(yearDir) if os.path.exists(yearDir) else None
+res = Dbase.conn.execute(
+    sqlalchemy.select(img, Thumbs.src, Thumbs.modified).where(
+    Thumbs.collection==Globals.currColl).order_by(
+    -Thumbs.modified)).fetchall()
 
-        for __b in __baseDirs:
-            for dirs in os.listdir(__b):
-                self.append(os.path.join(__b, dirs))
-    
-        self.__aged() if aged else None
-    
-    def __aged(self):
-        now = datetime.datetime.now().replace(microsecond=0)
-        delta = datetime.timedelta(days=int(cfg.FILE_AGE))
-        fileAge = now - delta
-        
-        oldDirs = list()
-        for s in self:
-            
-            birthFloat = os.stat(s).st_birthtime
-            birth = datetime.datetime.fromtimestamp(birthFloat)
+thumbs = []
+for blob, src, mod in res:
 
-            if  birth < fileAge:
-                oldDirs.append(s)                
-        
-        [self.remove(o) for o in oldDirs]
-                  
-                    
-class SearchColls(list):    
-    def __init__(self):
-        """List of dirs with cfg.COLL_FOLDER name."""
+    try:
+        nparr = numpy.frombuffer(blob, numpy.byte)
+        image1 = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
 
-        collsDirs = list()
-        for yearDir in BaseScan(aged=False):
-            if f'/{cfg.COLL_FOLDER}' in yearDir:
-                collsDirs.append(yearDir)
-                
-        for subColl in collsDirs:
-            for i in os.listdir(subColl):
-                self.append(os.path.join(subColl, i))
+        # convert cv2 color to rgb
+        imageRGB = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
 
+        # load numpy array image
+        image = Image.fromarray(imageRGB)
+        photo = ImageTk.PhotoImage(image)
+        year = datetime.fromtimestamp(mod).year
+        thumbs.append((photo, src, year))
 
-class SearchRetouched(list):
-    def __init__(self, aged=True):
-        """List of dirs with "retouch" name (from cfg.RT_FOLDER). 
-        :param bool aged: if true scan dirs with time created
-        earlier than number of days ago (cfg.FILE_AGE) only.""" 
-        
-        for dirItem in BaseScan(aged):
-            for root, _, _ in os.walk(dirItem):
-                print_alive(sys._getframe().f_code.co_name, root)
-                
-                if not cfg.FLAG:
-                    return
-                if os.path.join(os.sep, cfg.RT_FOLDER) in root:
-                    self.append(root)
-                    
+    except Exception:
+        print(traceback.format_exc())
 
-class SearchImages(list):
-    def __init__(self, listDirs):
-        """List of tuples (src, size, created, modified) of jpeg files. 
-        :param list lisrDirs: list of dirs for jpeg search."""
-        
-        allFiles = list()
-        
-        for path in listDirs:
-            for root, _, files in os.walk(path):
-                print_alive(sys._getframe().f_code.co_name, root)
-                
-                for file in files:
-                    allFiles.append(os.path.join(root, file))
-
-                    if not cfg.FLAG:
-                        return
-        
-        jpegs = set()
-        for src in allFiles:
-            
-            if not cfg.FLAG:
-                return
-            if src.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG')):
-                props = os.stat(src)                
-                size = int(os.path.getsize(src))
-                created = int(props.st_birthtime)
-                modified = int(props.st_mtime)
-                jpegs.add((src, size, created, modified))
-                
-        self.extend(jpegs)
-
-
-class DbUtils():
-    def CollName(self, src):
-        collName = 'noCollection'
-        if f'/{cfg.COLL_FOLDER}' in src:
-            collName = src.split(f'/{cfg.COLL_FOLDER}')[-1].split('/')[1]
-        return collName
-
-    def InsertRow(self, src, size, birth, mod, coll):
-        """Input: tuple > (src, size, created, mod, coll). 
-        Inserts new row in Database > Files. 
-        Requires CreateThumb object."""
-
-        img150, img200, img250, img300 = create_thumb(src)
-        
-        values = {
-            'img150':img150,
-            'img200':img200,
-            'img250':img250,
-            'img300':img300,
-            'src':src,
-            'size':size,
-            'created':birth,
-            'modified':mod,
-            'collection':coll
-            }
-
-        q = sqlalchemy.insert(Thumbs).values(values)
-        Dbase.conn.execute(q)
-
-
-class DbUpdate(list, DbUtils):
-    def __init__(self):
-        names =[
-            Thumbs.src, Thumbs.size, Thumbs.created, 
-            Thumbs.modified, Thumbs.collection
-            ]
-        q = sqlalchemy.select(names)
-        files = Dbase.conn.execute(q).fetchall()  
-        self.extend(files)
-    
-    def Removed(self):
-        for src, size, created, mod, coll in self:
-            print_alive(sys._getframe().f_code.co_name, src)
-
-            if not cfg.FLAG:
-                return
-            
-            if not os.path.exists(src):
-                print('removed file', src)
-                
-                self.remove((src, size, created, mod, coll))
-                q = sqlalchemy.delete(Thumbs).where(Thumbs.src==src)
-                Dbase.conn.execute(q)
-
-    def Modified(self):
-        for src, size, created, mod, coll in self:
-            atr = os.stat(src)
-            if int(atr.st_mtime) > mod:
-                print('modified', src)
-
-                nSize = int(os.path.getsize(src))
-                nCreated = int(atr.st_birthtime)
-                nMod = int(atr.st_mtime)
-                
-                self.remove((src, size, created, mod, coll))
-                q = sqlalchemy.delete(Thumbs).where(Thumbs.src==src)
-                Dbase.conn.execute(q)
-
-                coll = self.CollName(src)
-                self.append((src, nSize, nCreated, nMod, coll))                
-                self.InsertRow(src, nSize, nCreated, nMod, coll)
-
-    def Added(self, listDirs):
-        dbColls = list(
-            (size, creat, mod) for src, size, creat, mod, coll in self)
-
-        for src, size, created, mod in listDirs:
-            print_alive(sys._getframe().f_code.co_name, src)
-            
-            if not cfg.FLAG:
-                return
-
-            if (size, created, mod) not in dbColls:
-                print('add new file', src)
-
-                coll = self.CollName(src)
-                self.append((src, size, created, mod, coll))
-                self.InsertRow(src, size, created, mod, coll)    
-        
-    def Moved(self, lisrDirs):
-        noColls = list()
-
-        for src, size, created, mod, coll in self:
-            if coll=='noCollection':
-                noColls.append((size, created, mod))
-    
-        
-        for src, size, created, mod in lisrDirs:
-            print_alive(sys._getframe().f_code.co_name, src)
-            
-            if not cfg.FLAG:
-                return
-                
-            if (size, created, mod) in noColls:
-                print('moved to colls', src)
-                        
-                remRow = sqlalchemy.delete(Thumbs).where(
-                    Thumbs.size==size,
-                    Thumbs.created==created,
-                    Thumbs.modified==mod
-                    )
-                Dbase.conn.execute(remRow)
-
-                coll = self.CollName(src)
-                self.InsertRow(src, size, created, mod, coll)
-                
-
-class UpdateRt():
-    def __init__(self, aged):
-        images = SearchImages(SearchRetouched(aged))
-        
-        upd = DbUpdate()
-        upd.Removed()
-        upd.Modified()
-        upd.Added(images)
-
-class UpdateColl():
-    def __init__(self):
-        images = SearchImages(SearchColls())
-        
-        upd = DbUpdate()
-        upd.Removed()
-        upd.Modified()
-        upd.Added(images)
-        upd.Moved(images)
-
-
-UpdateColl()
-UpdateRt(aged=True)
+for i in thumbs:
+    print(i)
