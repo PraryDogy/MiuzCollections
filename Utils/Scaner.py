9bc0d13121fd95dea3352088ccf651fa7bea79cd
evlosh <evlosh@gmail.com>
2022-09-04 19:31:50 +0300

    lint

--- Utils/Scaner.py
+++ Utils/Scaner.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 import datetime
 import os
 import sys
@@ -7,105 +11,131 @@ import sqlalchemy
 import cfg
 from admin import print_alive
 from database import Thumbs, Dbase
-from Utils.Utils import create_thumb
+from utils.utils import create_thumb
 
 
-class BaseScan(list):    
-    def __init__(self, aged=True):
-        """List of dirs in folders with 
-        year name (year name is 2018, 2020 etc). 
-        :param bool aged: if true removes dirs with time created
-        earlier than number of days ago (cfg.FILE_AGE)""" 
-    
-        photoDir = os.path.join(os.sep, *cfg.PHOTO_DIR.split('/')) 
-        __baseDirs = list()
+class BaseScan(list):
+    """
+    Looking for folders with year names like "2018", "2020" etc.
+    in `cfg.PHOTO_DIR`
+    Looking for all folders in year named folders.
+    Returns list dirs.
+
+    * param `aged`: bool
+
+    `True` dirs from dirs list with time created earlier than
+    `cfg.FILE_AGE` days ago.
+    `False` returns dirs list with any time created.
+
+    `cfg.PHOTO_DIR` and `cfg.FILE_AGE` can be changed in
+    `cfg.json` (read `cfg.py`) or from app's settings.
+    """
+
+    def __init__(self, aged):
+        photo_dir = os.path.join(os.sep, *cfg.PHOTO_DIR.split('/'))
+        base_dirs = []
 
         for r in range(2018, datetime.datetime.now().year + 1):
-            yearDir = os.path.join(photoDir, str(r))
-            __baseDirs.append(yearDir) if os.path.exists(yearDir) else None
+            year_dir = os.path.join(photo_dir, str(r))
+            base_dirs.append(year_dir) if os.path.exists(year_dir) else False
 
-        for __b in __baseDirs:
-            for dirs in os.listdir(__b):
-                self.append(os.path.join(__b, dirs))
-    
-        self.__aged() if aged else None
-    
-    def __aged(self):
+        for b_dir in base_dirs:
+            for dirs in os.listdir(b_dir):
+                self.append(os.path.join(b_dir, dirs))
+
+        self.aged() if aged else False
+
+    def aged(self):
+        """
+        Removes from dirs list all dirs created earlier than
+        `cfg.FILE_AGE` days ago.
+        """
         now = datetime.datetime.now().replace(microsecond=0)
         delta = datetime.timedelta(days=int(cfg.FILE_AGE))
-        fileAge = now - delta
-        
-        oldDirs = list()
-        for s in self:
-            
-            birthFloat = os.stat(s).st_birthtime
-            birth = datetime.datetime.fromtimestamp(birthFloat)
+        file_age = now - delta
 
-            if  birth < fileAge:
-                oldDirs.append(s)                
-        
-        [self.remove(o) for o in oldDirs]
-                  
-                    
-class SearchColls(list):    
+        old_dirs = []
+        for dir_item in self:
+
+            birth_float = os.stat(dir_item).st_birthtime
+            birth = datetime.datetime.fromtimestamp(birth_float)
+
+            if  birth < file_age:
+                old_dirs.append(dir_item)
+
+        [self.remove(o) for o in old_dirs]
+
+
+class SearchColls(list):
+    """
+    Looking for folders with `cfg.COLL_FOLDER` name in list of dirs from
+    `BaseScan` with `aged=False` param.
+    Looking for all folders in `cfg.COLL_FOLDER`
+    """
     def __init__(self):
-        """List of dirs with cfg.COLL_FOLDER name."""
 
-        collsDirs = list()
-        for yearDir in BaseScan(aged=False):
-            if f'/{cfg.COLL_FOLDER}' in yearDir:
-                collsDirs.append(yearDir)
-                
-        for subColl in collsDirs:
-            for i in os.listdir(subColl):
-                self.append(os.path.join(subColl, i))
+        colls_dirs = list()
+        for year_dir in BaseScan(aged=False):
+            if os.path.join(os.sep, cfg.COLL_FOLDER) in year_dir:
+                colls_dirs.append(year_dir)
+
+        for sub_coll in colls_dirs:
+            for i in os.listdir(sub_coll):
+                self.append(os.path.join(sub_coll, i))
 
 
 class SearchRetouched(list):
-    def __init__(self, aged=True):
-        """List of dirs with "retouch" name (from cfg.RT_FOLDER). 
-        :param bool aged: if true scan dirs with time created
-        earlier than number of days ago (cfg.FILE_AGE) only.""" 
-        
-        for dirItem in BaseScan(aged):
-            for root, _, _ in os.walk(dirItem):
+    """
+    Looking for folders with `cfg.RT_FOLDER` name in list of dirs from
+    `BaseScan`.
+
+    * param `aged`: bool. Read BaseScan about `aged`.
+    """
+    def __init__(self, aged):
+
+        for dir_item in BaseScan(aged):
+            for root, _, _ in os.walk(dir_item):
                 print_alive(sys._getframe().f_code.co_name, root)
-                
+
                 if not cfg.FLAG:
                     return
+
                 if os.path.join(os.sep, cfg.RT_FOLDER) in root:
                     self.append(root)
-                    
+
 
 class SearchImages(list):
-    def __init__(self, listDirs):
-        """List of tuples (src, size, created, modified) of jpeg files. 
-        :param list lisrDirs: list of dirs for jpeg search."""
-        
-        allFiles = list()
-        
-        for path in listDirs:
+    """
+    Looking for `.jpeg` files in list of dirs.
+    Creates list of tuples with
+    `src`, int `size`, int `created`, int `modified`
+    """
+    def __init__(self, list_dirs):
+        all_files = []
+
+        for path in list_dirs:
             for root, _, files in os.walk(path):
                 print_alive(sys._getframe().f_code.co_name, root)
-                
+
                 for file in files:
-                    allFiles.append(os.path.join(root, file))
+                    all_files.append(os.path.join(root, file))
 
                     if not cfg.FLAG:
                         return
-        
+
         jpegs = set()
-        for src in allFiles:
-            
+        for src in all_files:
+
             if not cfg.FLAG:
                 return
+
             if src.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG')):
-                props = os.stat(src)                
+                attr = os.stat(src)
                 size = int(os.path.getsize(src))
                 created = int(props.st_birthtime)
                 modified = int(props.st_mtime)
                 jpegs.add((src, size, created, modified))
-                
+
         self.extend(jpegs)
 
 
