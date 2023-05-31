6d0398103b7ea0d3642ef25803a25d60ccddf881
Evgeny Loshkarev <evlosh@gmail.com>
2023-05-31 14:25:15 +0300

    grid

--- test.py
+++ test.py
@@ -1,8 +1,36 @@
-import subprocess
+import tkinter as tk
 
-args = (
-    "return user locale of (get system info)",
-    )
+root= tk.Tk()
 
-a = subprocess.check_output(["osascript", "-e", *args], text=True)
-print(a.split("\n"))
+title_f = tk.Frame(root)
+title_f.pack()
+cal_f = tk.Frame(root)
+cal_f.pack()
+
+month = tk.Label(title_f, text="Month", bg="yellow")
+year = tk.Label(title_f, text="Year", bg="yellow")
+
+for x, i in enumerate(("<", "Month", ">", "<", "Year", ">")):
+    arrow = tk.Label(title_f, text=i)
+    arrow.grid(row=0, column=x)
+
+r = 0
+c = 0
+for i in range(("Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс")):
+    weekkd = tk.Label(cal_f, text=str(i))
+    weekkd.grid(row=r, column=c)
+    c += 1
+
+r+= 1
+c = 0
+for i in range(1, 6*7+1):
+    day = tk.Label(cal_f, text=str(i))
+    day.grid(row=r, column=c)
+    c += 1
+    if c % 7 == 0:
+        c = 0
+        r += 1
+
+
+
+root.mainloop()
