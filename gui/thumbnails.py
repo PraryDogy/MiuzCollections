9a67900c53c47d3e3aacadb3c7fb9487d3268655
Morkik <evlosh@gmail.com>
2023-06-06 22:36:10 +0300

    bugs

--- gui/thumbnails.py
+++ gui/thumbnails.py
@@ -325,7 +325,7 @@ class ThumbnailsPrepare:
 
     def create_thumbs_dict(self):
         thumbs_dict = {}
-        limit = 100
+        limit = 500
         chunk_thumbs = [
             self.thumbs_lbls[i:i+limit]
             for i in range(0, len(self.thumbs_lbls), limit)
@@ -354,12 +354,15 @@ class ThumbnailsPrepare:
 
                 if len(chunk_thumbs) > 2:
                     thumbs_dict.setdefault(
-                        f"{chunk+1}: {key}", []
+                        f"{chunk}:{key}", []
                         ).append((img, src))
                 else:
                     thumbs_dict.setdefault(
                         key, []
                         ).append((img, src))
+
+        print(len(self.thumbs_lbls))
+        print(thumbs_dict.keys())
 
         return thumbs_dict
 
@@ -450,7 +453,7 @@ class Thumbnails(CFrame, ThumbnailsSearch, ThumbnailsPrepare):
             self.scroll_frame, bg=conf.bg_color, scrollbarwidth=7)
         self.sframe.pack(expand=1, fill=tkinter.BOTH)
 
-        self.scroll_frame.bind_all("<ButtonRelease-1>", self.g_click)
+        # self.scroll_frame.bind_all("<ButtonRelease-1>", self.g_click)
 
     def g_click(e: tkinter.Event=None, ee: tkinter.Event=None):
         try:
@@ -520,6 +523,7 @@ class Thumbnails(CFrame, ThumbnailsSearch, ThumbnailsPrepare):
         self.search_frame(title_frame).pack(pady=(15, 0), ipady=2)
 
         for dates, img_list in self.thumbs_lbls.items():
+            dates = dates.split(":")[-1]
             thumbs_title = CLabel(
                 self.thumbs_frame,
                 text=(
