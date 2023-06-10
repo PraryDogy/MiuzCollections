ee4b52186a6ce5435f946bb338956c34f0a5f3c5
Morkik <evlosh@gmail.com>
2023-06-10 13:27:17 +0300

    menu refactoring

--- gui/menu.py
+++ gui/menu.py
@@ -6,51 +6,36 @@ __all__ = (
     "Menu",
     )
 
-menu_buttons = []
+
+class MenuExtend:
+    def open_finder(self, e):
+        if e.widget.true_name != conf.all_colls:
+            coll_path = os.path.join(conf.coll_folder, e.widget.true_name)
+        else:
+            coll_path = conf.coll_folder
+
+        subprocess.check_output(["/usr/bin/open", coll_path])
 
 
-def open_finder(collection_name):
-    if collection_name != conf.all_colls:
-        coll_path = os.path.join(conf.coll_folder, collection_name)
-    else:
-        coll_path = conf.coll_folder
-
-    subprocess.check_output(["/usr/bin/open", coll_path])
-
-
-def show_collection(master: tkinter.Button, collection_name):
-    for btn_item in menu_buttons:
-        btn_item['bg'] = conf.btn_color
-
-    master['bg'] = conf.sel_color
-    conf.curr_coll = collection_name
-
-    from .thumbnails import Thumbnails, ThumbSearch
-    ThumbSearch.reload_search()
-    Thumbnails.reload_with_scroll()
-
-
-class ContextMenu(tkinter.Menu):
-    def __init__(self, master, collection_name):
-        super().__init__(master)
-        self.btn = master
-        self.collection_name = collection_name
+class ContextMenu(tkinter.Menu, MenuExtend):
+    def __init__(self, e: tkinter.Event):
+        super().__init__(e.widget)
 
         self.add_command(
-            label = conf.lang.show_finder,
-            command = lambda: open_finder(collection_name)
+            label=conf.lang.show_finder,
+            command=lambda: self.open_finder(e)
             )
 
-        master.bind("<Button-2>", self.do_popup)
+        self.do_popup(e)
 
-    def do_popup(self, event):
+    def do_popup(self, e):
         try:
-            self.tk_popup(event.x_root, event.y_root)
+            self.tk_popup(e.x_root, e.y_root)
         finally:
             self.grab_release()
 
 
-class Menu(tkmacosx.SFrame):
+class Menu(tkmacosx.SFrame, MenuExtend):
     reload_menu = None
     def __init__(self, master: tkinter):
         super().__init__(
@@ -60,22 +45,13 @@ class Menu(tkmacosx.SFrame):
             width = conf.menu_w
             )
 
-        self.menu_parrent = self.load_menu_parent()
-        self.menu_parrent.pack()
+        self.menu_buttons = []
+        self.sel_btn = tkinter.Label
 
-        self.menu_buttons = self.load_menu_buttons()
-        self.menu_buttons.pack()
+        self.menu_frame = self.load_menu_buttons()
+        self.menu_frame.pack()
 
         __class__.reload_menu = self.__reload_menu
-
-    def load_menu_parent(self):
-        frame = CFrame(self)
-        frame.pack()
-
-        menu_frame = CFrame(frame)
-        menu_frame.pack(side=tkinter.BOTTOM)
-
-        return frame
 
     def re_collname(self, coll: str):
         try:
@@ -103,20 +79,18 @@ class Menu(tkmacosx.SFrame):
             for coll in colls_list
             }
 
-        menus = dict(
-            sorted(
-                menus.items(),
-                key = lambda item: item[1].casefold()
-                ))
+        menus = sorted(menus.items(), key=lambda item: item[1].casefold())
+        menus = dict(menus)
 
         last = CButton(frame, text=conf.lang.all_colls)
         last.configure(width=13, pady=5, anchor=tkinter.W, padx=10)
-        last.cmd(partial(self.open_coll_folder, conf.all_colls, last))
+        last.true_name = conf.all_colls
+        last.cmd(self.show_coll)
         last.pack(pady=(0, 15))
-        menu_buttons.append(last)
-        conf.lang_menu.append(last)
+        last.bind("<Button-2>", ContextMenu)
 
-        ContextMenu(last, conf.all_colls)
+        self.menu_buttons.append(last)
+        conf.lang_menu.append(last)
 
         sep = CSep(frame)
         sep['bg'] = '#272727'
@@ -125,14 +99,16 @@ class Menu(tkmacosx.SFrame):
         for collection_name, menu_name in menus.items():
             btn = CButton(frame, text = menu_name)
             btn.configure(width=13, pady=5, anchor=tkinter.W, padx=10)
-            btn.cmd(partial(self.open_coll_folder, collection_name, btn))
+            btn.true_name = collection_name
+            btn.cmd(self.show_coll)
             btn.pack()
-            menu_buttons.append(btn)
+            btn.bind("<Button-2>", ContextMenu)
 
-            ContextMenu(btn, collection_name)
+            self.menu_buttons.append(btn)
 
             if collection_name == conf.curr_coll:
                 btn.configure(bg=conf.sel_color)
+                self.sel_btn = btn
 
             sep = CSep(frame)
             sep['bg'] = '#272727'
@@ -140,17 +116,26 @@ class Menu(tkmacosx.SFrame):
     
         if conf.curr_coll == conf.all_colls:
             last.configure(bg=conf.sel_color)
+            self.sel_btn = last
 
         return frame
 
     def __reload_menu(self):
-        menu_buttons.clear()
+        self.menu_buttons.clear()
         conf.lang_menu.clear()
-        self.menu_buttons.destroy()
-        self.menu_buttons = self.load_menu_buttons()
-        self.menu_buttons.pack()
+        self.menu_frame.destroy()
+        self.menu_frame = self.load_menu_buttons()
+        self.menu_frame.pack()
         return
     
-    def open_coll_folder(self, coll: str, btn: CButton, e):
+    def show_coll(self, e):
         conf.limit = 150
-        show_collection(btn, coll)
+
+        self.sel_btn.configure(bg=conf.btn_color)
+        e.widget.configure(bg=conf.sel_color)
+        self.sel_btn = e.widget
+        conf.curr_coll = e.widget.true_name
+
+        from .thumbnails import Thumbnails, ThumbSearch
+        ThumbSearch.reload_search()
+        Thumbnails.reload_with_scroll()
