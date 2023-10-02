d87b106c36a37382873ab386d1df168b80528606
Evgeny Loshkarev <evlosh@gmail.com>
2023-10-02 11:33:52 +0300

    123

--- gui/application.py
+++ gui/application.py
@@ -15,7 +15,7 @@ class Application:
         conf.root.bind('<Command-w>', self.minim)
         conf.root.protocol("WM_DELETE_WINDOW", self.minim)
         conf.root.createcommand("tk::mac::Quit" , on_exit)
-        conf.root.createcommand('tk::mac::ReopenApplication', conf.root.deiconify)
+        # conf.root.createcommand('tk::mac::ReopenApplication', conf.root.deiconify)
 
 
         menu = Menu(conf.root)
