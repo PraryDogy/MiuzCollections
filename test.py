1e440b1c8586a9a9183ac602d547f03dcb652340
Morkik <evlosh@gmail.com>
2023-10-18 23:44:07 +0300

    working save tiff

--- test.py
+++ test.py
@@ -2,18 +2,21 @@ from PIL import Image, UnidentifiedImageError
 from psd_tools import PSDImage
 import cv2
 import tifffile
-import imageio.v3 as iio
-import numpy as np
-from array import array
-import io
 
 src = "/Volumes/Shares/Marketing/Photo/_Collections/2 Flaming Ice/1 IMG/E2018-ICE-0010_R2018-ICE-0009.psd"
 src = "/Volumes/Shares/Marketing/Photo/_Collections/10 Brilliance/1 IMG/2023-10-09 22-06-29_E01-SS-35613.tif"
 dest = "/Users/Loshkarev/Downloads/new.jpg"
 
-if src.split("/")[-1].split(".")[-1] in ("psd", "PSD"):
+src = "/Users/Morkowik/Downloads/E2018-ICE-0010_R2018-ICE-0009.psd"
+# src = "/Users/Morkowik/Downloads/2023-10-09 22-06-29_E01-SS-35613.tif"
+dest = "/Users/Morkowik/Downloads/new.jpg"
+
+if src.endswith((".psd", ".PSD")):
     img = PSDImage.open(src)
-    img = img.composite().save(dest)
+    img.composite().save(dest)
 else:
     img = tifffile.imread(src)
-    img.compress(i)
+    img = img[:,:,:3]
+    b,g,r = cv2.split(img)
+    img = cv2.merge([r, g, b])
+    cv2.imwrite(dest, img)
