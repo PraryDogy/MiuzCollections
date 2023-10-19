96ac01e2cc2000bff160f5da6de8cd22a6324b8f
Evgeny Loshkarev <evlosh@gmail.com>
2023-10-19 12:09:02 +0300

    123

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import cv2
 import tifffile
 
 
-# src = "/Users/Loshkarev/Downloads/15ch16bit.tif"
+src = "/Users/Loshkarev/Downloads/15ch16bit.tif"
 # src = "/Users/Loshkarev/Downloads/15ch8bit.tif"
-src = "/Users/Loshkarev/Downloads/3ch16bit.tif"
+# src = "/Users/Loshkarev/Downloads/3ch16bit.tif"
 
 dest = "/Users/Loshkarev/Downloads/new.jpg"
 
@@ -18,15 +18,22 @@ else:
     img = tifffile.imread(src)
     channels = cv2.split(img)
 
-    if len(channels) > 3:
-        print("more than 3 channels")
-        img = img[:,:,:3]
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(dest, img)
-    else:
-        img.shape
-        img.dtype
-        img.imsave(dest, img)
+    ch, bit = len(cv2.split(img)), img.dtype
+
+
+    print(ch, bit)
+
+    # if len(channels) > 3:
+    #     print("more than 3 channels")
+    #     img = img[:,:,:3]
+    #     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #     cv2.imwrite(dest, img)
+    # else:
+    #     img.shape
+    #     img.dtype
+    #     tifffile.imwrite(dest, img)
+
+    tifffile.imwrite(dest, img)
 
     # cv2.imshow("1", img)
     # cv2.waitKey(0) 
