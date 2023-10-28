75cbf183fadc579116f9954cd4d59d30cae68ad7
Morkik <evlosh@gmail.com>
2023-10-28 18:23:22 +0300

    ref

--- gui/img_viewer.py
+++ gui/img_viewer.py
@@ -17,178 +17,155 @@ __all__ = (
     )
 
 
-src = str
-all_src = []
-win = tkinter.Toplevel
-
-
 class ContextViewer(Context):
-    def __init__(self, e: tkinter.Event):
+    def __init__(self, e: tkinter.Event, img_src):
         super().__init__()
 
         if cnf.first_load:
             self.please_wait()
 
         else:
-            self.imginfo(e)
+            self.imginfo(img_src)
             self.sep()
 
             self.sep()
-            self.copy_jpg_path(e)
-            self.reveal_jpg(e)
-            self.download_onefile(e)
+            self.copy_jpg_path(img_src)
+            self.reveal_jpg(img_src)
+            self.download_onefile(img_src)
 
             self.sep()
-            self.copy_tiffs_paths(e)
-            self.reveal_tiffs(e)
-            self.download_tiffs(e)
+            self.copy_tiffs_paths(img_src)
+            self.reveal_tiffs(img_src)
+            self.download_tiffs(img_src)
 
             self.sep()
-            self.download_fullsize(e)
+            self.download_fullsize(img_src)
 
         self.do_popup(e)
 
 
 class ImgViewer(CWindow):
-    def __init__(self, img_src: str, src_list: list):
-        global src, all_src, win
-        super().__init__()
+    def __init__(self, img_src: str, all_src: list):
+        super().__init__(bg="black", pady=0, padx=0)
 
-        win = self
-        src = img_src
-        all_src = src_list
+        self.img_src = img_src
+        self.all_src = all_src
 
         self.set_title()
-        self.configure(bg="black")
-
         self.geometry(f"{cnf.imgview_g['w']}x{cnf.imgview_g['h']}")
         self.minsize(500, 300)
-
-        self.configure(pady=0, padx=0)
         self.resizable(1, 1)
 
-        self.img_frame = self.img_widget()
-        self.img_frame.pack()
+        self.img = CLabel(
+            self, bg="black", width=cnf.imgview_g["w"],
+            height=cnf.imgview_g["h"]
+            )
+        self.img.bind("<ButtonRelease-1>", lambda e: self.l_click(e))
+        self.img.bind(
+            "<ButtonRelease-2>",
+            lambda e, img_src=self.img_src: ContextViewer(e, img_src)
+            )
+        self.img.pack()
 
         cnf.root.update_idletasks()
 
-        self.img_frame.configure(
-            width=cnf.imgview_g["w"], height=cnf.imgview_g["h"]
-            )
-
-        self.thumb_place(cnf.imgview_g["w"], cnf.imgview_g["h"])
-        self.task = cnf.root.after(
-            250, lambda: self.img_place(cnf.imgview_g["w"], cnf.imgview_g["h"]))
+        self.load_thumb()
+        self.img_task = cnf.root.after(550, self.load_img)
+        self.resize_task = None
 
         place_center()
         self.deiconify()
         self.wait_visibility()
         self.grab_set_global()
 
+        self.bind(
+            "<Left>",
+            lambda e: self.switch_img(self.all_src.index(self.img_src)-1)
+            )
+        self.bind(
+            "<Right>",
+            lambda e: self.switch_img(self.all_src.index(self.img_src)+1)
+            )
         self.bind("<Configure>", self.decect_resize)
-        self.bind("<ButtonRelease-2>", self.r_click)
-        self.resize_task = None
 
-    def r_click(self, e):
-        e.widget.src = src
-        e.widget.all_src = all_src
-        ContextViewer(e)
-
-    def decect_resize(self, e):
+    def decect_resize(self, e: tkinter.Event):
         if self.resize_task:
             cnf.root.after_cancel(self.resize_task)
-        self.resize_task = cnf.root.after(250, lambda: self.resize_win())
+        self.resize_task = cnf.root.after(550, lambda: self.resize_win(e))
 
-    def resize_win(self):
+    def resize_win(self, e: tkinter.Event):
         try:
-            new_w, new_h = self.winfo_width(), self.winfo_height()
-        except Exception:
-            print("no win")
-            return
+            if e.width != cnf.imgview_g["w"] or e.height != cnf.imgview_g["h"]:
+                cnf.imgview_g["h"] = e.height
+                cnf.imgview_g["w"] = e.width
 
-        if new_w != cnf.imgview_g["w"] or new_h != cnf.imgview_g["h"]:
-            cnf.imgview_g["h"] = new_h
-            cnf.imgview_g["w"] = new_w
+                self.img.configure(
+                    width=cnf.imgview_g["w"], height=cnf.imgview_g["h"]
+                    )
 
-            self.img_frame.configure(
-                width=cnf.imgview_g["w"],
-                height=cnf.imgview_g["h"]
+                self.load_thumb()
+                cnf.root.after(550, self.load_img)
+        except Exception as ex:
+            print("img viewer > resize win > no win")
+            print(ex)
+
+    def load_thumb(self):
+        img = Dbase.conn.execute(sqlalchemy.select(Thumbs.img150).where(
+            Thumbs.src==self.img_src)).first()[0]
+        img = decode_image(img)
+        img = resize_image(
+            img, cnf.imgview_g["w"], cnf.imgview_g["h"], thumbnail=False
+            )
+        img = convert_to_rgb(img)
+        self.tk_img(img)
+
+    def load_img(self):
+        try:
+            img = cv2.imread(self.img_src, cv2.IMREAD_UNCHANGED)
+            
+            if self.img_src.endswith(("png", "PNG")):
+                img = replace_bg(img, cnf.bg_color)
+
+            img = resize_image(
+                img, cnf.imgview_g["w"], cnf.imgview_g["h"], thumbnail=False
                 )
+            img = convert_to_rgb(img)
+            self.tk_img(img)
 
-            self.thumb_place(cnf.imgview_g["w"], cnf.imgview_g["h"])
-            cnf.root.after(
-                500,
-                lambda: self.img_place(cnf.imgview_g["w"], cnf.imgview_g["h"])
-                )
+        except Exception as ex:
+            print("img viewer > img place > no win")
+            print(ex)
 
-    def img_widget(self):
-        label = CLabel(self, bg="black")
-        label.bind("<ButtonRelease-1>", lambda e: self.img_click(e))
-        self.bind("<Left>", lambda e: self.switch_img(self.img_ind()-1))
-        self.bind("<Right>", lambda e: self.switch_img(self.img_ind()+1))
-        return label
+    def tk_img(self, img):
+        img_tk = ImageTk.PhotoImage(img)
+        self.img.configure(image=img_tk)
+        self.img.image_names = img_tk
 
     def switch_img(self, ind: int):
-        global src
-
-        cnf.root.after_cancel(self.task)
+        cnf.root.after_cancel(self.img_task)
         try:
-            src = all_src[ind]
-            self.set_title()
+            self.img_src = self.all_src[ind]
         except IndexError:
-            src = all_src[0]
-            self.set_title()
+            self.img_src = self.all_src[0]
 
-        self.thumb_place(cnf.imgview_g["w"], cnf.imgview_g["h"])
-        self.task = cnf.root.after(
-            500,
-            lambda: self.img_place(cnf.imgview_g["w"], cnf.imgview_g["h"])
+        self.set_title()
+        self.bind(
+            "<ButtonRelease-2>",
+            lambda e, img_src=self.img_src: ContextViewer(e, img_src)
             )
 
-    def img_ind(self):
-        return all_src.index(src)
+        self.load_thumb()
+        self.img_task = cnf.root.after(500, self.load_img)
 
-    def img_set(self, img):
-        img_tk = ImageTk.PhotoImage(img)
-        self.img_frame.configure(image=img_tk)
-        self.img_frame.image_names = img_tk
-
-    def img_click(self, e: tkinter.Event):
-        if cnf.imgview_g["w"] == self.winfo_width():
-
-            if e.x <= cnf.imgview_g["w"]//2:
-                index = self.img_ind() - 1
-            else:
-                index = self.img_ind() + 1
-
-            self.switch_img(index)
-
-    def thumb_load(self):
-        thumb = Dbase.conn.execute(sqlalchemy.select(Thumbs.img150).where(
-            Thumbs.src==src)).first()[0]
-        return decode_image(thumb)
-
-    def thumb_place(self, width, height):
-        thumb = self.thumb_load()
-        resized = resize_image(thumb, width, height, False)
-        rgb_thumb = convert_to_rgb(resized)
-        self.img_set(rgb_thumb)
-
-    def img_place(self, width, height):
-        try:
-            img_read = cv2.imread(src, cv2.IMREAD_UNCHANGED)
-            
-            if src.endswith(("png", "PNG")):
-                img_read = replace_bg(img_read, cnf.bg_color)
-
-            resized = resize_image(img_read, width, height, False)
-            img_rgb = convert_to_rgb(resized)
-            self.img_set(img_rgb)
-        except AttributeError:
-            print("img viewer no img\n\n")
+    def l_click(self, e: tkinter.Event):
+        if e.x <= cnf.imgview_g["w"]//2:
+            index = self.all_src.index(self.img_src) - 1
+        else:
+            index = self.all_src.index(self.img_src) + 1
+        self.switch_img(index)
 
     def set_title(self):
-        name = src.split(os.sep)[-1]
-        collection_name = get_coll_name(src)
+        name = self.img_src.split(os.sep)[-1]
+        collection_name = get_coll_name(self.img_src)
         self.title(f"{collection_name} - {name}")
 
