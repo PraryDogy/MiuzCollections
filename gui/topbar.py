e4d9dc2ad7e85ac4503144b1b239c48dacf5f6ba
Evgeny Loshkarev <evlosh@gmail.com>
2023-11-17 14:30:56 +0300

    ref

--- gui/topbar.py
+++ gui/topbar.py
@@ -27,7 +27,7 @@ class SearchWid(CEntry):
                         textvariable=textvariable, **kw)
 
         self.bind(sequence="<Escape>", command=lambda e: cnf.root.focus_force())
-        cnf.root.bind(sequence="<Command-f>", command=lambda e: self.focus_force())
+        cnf.root.bind(sequence="<Command-f>", func=lambda e: self.focus_force())
 
         self.bind(sequence="<Return>", command=self.__search_go)
         self.bind(sequence="<ButtonRelease-2>", command=ContextSearch)
@@ -54,27 +54,27 @@ class SearchWid(CEntry):
     def __search_go(self, e=None):
         self.__cancel_search_task()
         self.__old_search_var = cnf.search_var.get()
-        cnf.search_var.set(self.get())
+        cnf.search_var.set(value=self.get())
         cnf.start, cnf.end = None, None
         cnf.reload_scroll()
         self.focus_force()
 
 
 class FiltersWid(CFrame):
-    def __init__(self, master: tkinter, **kw):
-        super().__init__(master, **kw)
+    def __init__(self, master: tkinter, bg: str = cnf.bg_color, **kwargs):
+        CFrame.__init__(self, master=master, bg=bg, **kwargs)
 
-        self.filter_btns = {}
+        self.__filter_btns = {}
 
         for k, v in cnf.lng.filter_names.items():
-            btn = CButton(self, text=cnf.lng.filter_names[k])
+            btn = CButton(master=self, text=cnf.lng.filter_names[k])
             btn.pack(side="left", fill="x", padx=(0, 5))
-            self.filter_btns[btn] = k
+            self.__filter_btns[btn] = k
 
-        for k, v in self.filter_btns.items():
-            k.cmd(lambda e, v=v: self.__filter_btn_cmd(v))
+        for k, v in self.__filter_btns.items():
+            k.cmd(lambda e, v=v: self.__filter_btn_cmd(v=v))
 
-        self.dates_btn = CButton(self, text=cnf.lng.dates)
+        self.dates_btn = CButton(master=self, text=cnf.lng.dates)
         self.dates_btn.pack(side="left", fill="x", padx=(0, 5))
         self.dates_btn.cmd(lambda e: Filter())
 
@@ -86,50 +86,45 @@ class FiltersWid(CFrame):
         cnf.reload_scroll()
 
     def reload_filters(self):
-        for k, v in self.filter_btns.items():
+        for k, v in self.__filter_btns.items():
             k: CButton
             if cnf.filter_values[v]:
                 k.configure(fg_color=cnf.btn_color,
                             text=cnf.lng.filter_names[v] + " ⨂")
             else:
                 k.configure(fg_color=cnf.bg_color,
-                    text=cnf.lng.filter_names[v] + " ⨁")
+                            text=cnf.lng.filter_names[v] + " ⨁")
 
         if any((cnf.start, cnf.end)):
-            self.dates_btn.configure(
-                fg_color=cnf.btn_color,
-                text=cnf.lng.dates + " ⨂"
-                )
+            self.dates_btn.configure(fg_color=cnf.btn_color,
+                                     text=cnf.lng.dates + " ⨂")
         else:
-            self.dates_btn.configure(
-                fg_color=cnf.bg_color,
-                text=cnf.lng.dates + " ⨁"
-                )
+            self.dates_btn.configure(fg_color=cnf.bg_color,
+                                     text=cnf.lng.dates + " ⨁")
 
 
 class FuncBar(CFrame):
-    def __init__(self, master: tkinter, **kw):
-        super().__init__(master, **kw)
+    def __init__(self, master: tkinter, bg: str = cnf.bg_color, **kwargs):
+        CFrame.__init__(self, master=master, bg=bg, **kwargs)
 
         if cnf.curr_coll == cnf.all_colls:
             coll_title = cnf.lng.all_colls
         else:
             coll_title = cnf.curr_coll
 
-        first_row = CFrame(self)
+        first_row = CFrame(master=self)
         first_row.pack(fill="x", pady=(0, 5))
-        second_row = CFrame(self)
+        second_row = CFrame(master=self)
         second_row.pack(fill="x")
 
-        self.topbar_title = CLabel(
-            first_row, text=coll_title, anchor="w",
-            font=("San Francisco Pro", 22, "bold"))
-        self.topbar_title.pack(anchor="w", side="left")
+        self.__topbar_title = CLabel(master=first_row, text=coll_title, anchor="w", 
+                                     font=("San Francisco Pro", 22, "bold"))
+        self.__topbar_title.pack(anchor="w", side="left")
 
-        search = SearchWid(first_row)
+        search = SearchWid(master=first_row)
         search.pack(anchor="e", side="right")
 
-        self.filters_wid = FiltersWid(second_row)
+        self.filters_wid = FiltersWid(master=second_row)
         self.filters_wid.pack(anchor="w")
 
     def set_topbar_title(self):
@@ -138,46 +133,47 @@ class FuncBar(CFrame):
         else:
             coll_title = cnf.curr_coll
 
-        self.topbar_title.configure(text=coll_title)
+        self.__topbar_title.configure(text=coll_title)
+
+   
 
 
 class NotifyBar(CFrame):
-    def __init__(self, master: tkinter, fg_color=cnf.bg_color, **kw):
-        self.fg_color = fg_color
-        super().__init__(master, bg=fg_color, **kw)
+    def __init__(self, master: tkinter, bg: str = cnf.bg_color, **kwargs):
+        CFrame.__init__(self, master=master, bg=bg, **kwargs)
 
-        self.btn_up = CButton(self, text=f"▲", fg_color=fg_color)
-        self.btn_up.pack(side="left", fill="x", expand=1)
-        self.btn_up.cmd(lambda e: cnf.move_up())
+        self.__fg_color = bg
 
-    def notibar_text(self, text):
+        self.__btn_up = CButton(self, text=f"▲", fg_color=bg)
+        self.__btn_up.pack(side="left", fill="x", expand=1)
+        self.__btn_up.cmd(lambda e: cnf.move_up())
+
+    def notibar_text(self, text: str):
         try:
-            self.btn_up.configure(text=text, fg_color=cnf.blue_color)
+            self.__btn_up.configure(text=text, fg_color=cnf.blue_color)
             if len(self.children) < 2:
-                self.topbar_can = CButton(
-                    self, text=cnf.lng.cancel, fg_color=cnf.blue_color,
-                    )
-                self.topbar_can.configure()
-                self.topbar_can.cmd(lambda e: cancel_utils_task())
-                self.topbar_can.pack(side="left", padx=(1, 0))
+                self.__topbar_can = CButton(master=self, text=cnf.lng.cancel,
+                                          fg_color=cnf.blue_color)
+                self.__topbar_can.cmd(lambda e: cancel_utils_task())
+                self.__topbar_can.pack(side="left", padx=(1, 0))
         except RuntimeError as e:
             print("thumbnails > topbar text error")
             print(e)
 
     def notibar_default(self):
         try:
-            self.topbar_can.destroy()
+            self.__topbar_can.destroy()
         except AttributeError as e:
             print("thumbnails > no topbar cancel button")
             print(e)
         try:
-            self.btn_up.configure(text=f"▲", fg_color=self.fg_color)
+            self.__btn_up.configure(text=f"▲", fg_color=self.__fg_color)
         except RuntimeError as e:
             print("thumbnails > can't configure topbar to default")
             print(e)
 
 
-class TopBar(NotifyBar):
+class TopBar(CFrame):
     def __init__(self, master: tkinter):
         CFrame.__init__(self, master=master)
 
