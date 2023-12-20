784b9e5dcf382ea81a9f53a0f3d46ba158b48ac6
Evgeny Loshkarev <evlosh@gmail.com>
2023-12-20 10:41:40 +0300

    setup mod

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ import icnsutil
 from setuptools import setup
 
 from cfg import cnf
+from setup_ext import SetupExt
 
 src = "icon.png"
 img = icnsutil.IcnsFile()
@@ -41,32 +42,4 @@ setup(
     install_requires = []
     )
 
-ver = "3.11"
-lib_src = f"/Library/Frameworks/Python.framework/Versions/{ver}/lib"
-folders = "tcl8", "tcl8.6", "tk8.6"
-
-for i in folders:
-    shutil.copytree(
-        os.path.join(lib_src, i),
-        os.path.join(f"dist/{cnf.app_name}.app/Contents/lib", i)
-        )
-
-folder = os.path.join(os.path.expanduser("~/Desktop"), cnf.app_name)
-
-if not os.path.exists(folder):
-    os.mkdir(folder)
-else:
-    shutil.rmtree(folder)
-    os.mkdir(folder)
-
-subprocess.Popen(
-    ["ln", "-s", "/Applications", os.path.join(folder, "Программы")]
-        )
-shutil.move(f"dist/{cnf.app_name}.app", f"{folder}/{cnf.app_name}.app")
-
-
-shutil.rmtree("build")
-shutil.rmtree(".eggs")
-shutil.rmtree("dist")
-
-subprocess.Popen(["open", "-R", folder])
+SetupExt(py_ver="3.11", appname=cnf.app_name)
