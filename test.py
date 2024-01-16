9ca623d737a68a8529b24699f74f88429011d071
Morkik <evlosh@gmail.com>
2024-01-16 21:52:37 +0300

    modified pathfinder

--- test.py
+++ test.py
@@ -2,29 +2,36 @@ import os
 
 path = "smb://sbc01/shares/Marketing/Photo/_Collections/1 Solo/1 IMG/2023-09-22 11-27-28 рабочий файл.tif/"
 # path = "\\192.168.10.105\\shares\\Marketing\\General\\9. ТЕКСТЫ\\2023\\7. PR-рассылка\\10. Октябрь\\Royal"
+path = "/Users/Morkowik/Downloads/Геохимия видео"
 
-class PathFinder(list):
+
+class PathFinder(object):
     def __init__(self, path: str):
-        list.__init__(self)
         pre_paths = ["/Volumes/Shares/Marketing", ] # from json
+        pre_paths = [self.normalize_path(path=i)
+                    for i in pre_paths]
 
-        path = path.replace("\\", os.sep).strip().strip(os.sep)
+        path = self.normalize_path(path=path)
+
         path_list = path.split(os.sep)
-        path_versions = []
 
-        for pre_path in pre_paths:
-            for i in range(len(path_list)):
-                path_versions.append(os.path.join(pre_path, *path_list[i:]))
+        path_versions = (os.path.join(pre_path, *path_list[i:])
+                         for pre_path in pre_paths
+                         for i in range(len(path_list)))
 
+        self.path_exists = None
         for i in path_versions:
             if os.path.exists(i):
-                self.append(i)
-                break
+                self.path_exists = i
+                return
+            
+    def normalize_path(self, path: str):
+        path = path.replace("\\", os.sep).strip().strip(os.sep)
+        path = path.split(os.sep)
+        return os.path.join(os.sep, *path)
 
-    def __str__(self) -> str:
-        try:
-            return self[0]
-        except Exception:
-            return False
 
-print(PathFinder(path=path))
+
+a = PathFinder(path=path)
+
+print(a.path_exists)
