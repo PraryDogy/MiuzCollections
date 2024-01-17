a30e8ff30cf0d3ee98dceaa1e5411a5c0a038e37
Evgeny Loshkarev <evlosh@gmail.com>
2024-01-17 12:44:13 +0300

    fix?

--- utils/scaner_new.py
+++ utils/scaner_new.py
@@ -218,7 +218,7 @@ class UpdateDb(SetUpdateStatus, SysUtils):
 class ScanerBase(UpdateDb):
     def __init__(self):
         UpdateDb.__init__(self)
-        OldCollFolderRemover()
+        # OldCollFolderRemover()
         DublicateRemover()
 
 
